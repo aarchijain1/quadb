661b08a3dbfd4b539da3f50578551524ba78ae50
Aarchi Jain <aarchi.jain0409@gmail.com>
2024-07-12 19:04:06 +0530

    first commit

---
+++ src/components/Form.js
@@ -0,0 +1,71 @@
+import React,{useState, useEffect} from 'react'
+import { useDispatch } from 'react-redux';
+import { addTodo, handleEditSubmit } from '../redux/todoapp/actions';
+
+export const Form = ({editFormVisibility, editTodo, cancelUpdate}) => {
+
+  // dispatch function to dispatch an action
+  const dispatch = useDispatch();
+
+  // todo value state for normal add todo form
+  const [todoValue, setTodoValue]=useState('');
+
+  // state for if someone changes the (to edit) value in update form
+  const [editValue, setEditValue]=useState('');
+
+  // useEffect is to show the (to edit) value in update form
+  useEffect(()=>{
+    setEditValue(editTodo.todo);
+  },[editTodo])
+
+  // normal add todo submit
+  const handleSubmit=(e)=>{
+      e.preventDefault();
+      let date = new Date();
+      let time = date.getTime();
+      let todoObj={
+          id: time,
+          todo: todoValue,
+          completed: false
+      }
+      setTodoValue('');
+      dispatch(addTodo(todoObj))
+  }
+
+  // update form submit
+  const editSubmit = (e) =>{
+    e.preventDefault();
+    let editedObj={
+      id: editTodo.id,
+      todo: editValue,
+      completed: false
+    }
+    dispatch(handleEditSubmit(editedObj))
+  }
+
+  return (
+    <>
+      {editFormVisibility===false?(
+        <form className='form-group custom-form text-white' onSubmit={handleSubmit} id='form1'>
+          <label t>Add your todo-items</label>
+          <div className='input-and-btn'>
+              <input type="text" id='ip2' className='form-control' required
+              value={todoValue} onChange={(e)=>setTodoValue(e.target.value)}/>
+              <button type="submit" className='btn btn-primary btn-md todo-button text-white '>ADD</button>
+          </div>
+        </form>
+      ):(
+        <form id='f2' className='form-group custom-form' onSubmit={editSubmit}>
+          <label>Update your todo-items</label>
+          <div className='input-and-btn'>
+              <input type="text" id='ip3' className='form-control' required
+              value={editValue||""} onChange={(e)=>setEditValue(e.target.value)}/>
+              <button type="submit" className='btn btn-secondary btn-md'>UPDATE</button>
+          </div>
+          <button type="button" className='btn btn-primary btn-md back-btn'
+          onClick={cancelUpdate}>BACK</button>
+        </form>
+      )}
+    </>
+  )
+}
